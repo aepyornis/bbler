c640e394a15c3661e62c6823f3326b58180fd1ad
ziggy <ziggy@elephant-bird.net>
2018-04-08 10:38:45 -0400

    retry failed dob jobs

--- lib/job_download.py
+++ lib/job_download.py
@@ -9,26 +9,39 @@ JOB_URL = "http://a810-bisweb.nyc.gov/bisweb/JobsQueryByNumberServlet?passjobnum
 USER_AGENT = 'Mozilla/5.0'
 HEADERS = {'User-Agent': USER_AGENT }
 FOLDER = 'data'
+UNAVAILABLE = 'Due to the high demand it may take a little longer'
 # seconds between download
 THROTTLE = 8
-
+PAUSE_BEFORE_RETRY = 15
 
 def job_url(job_number):
     return JOB_URL.format(job_number)
 
 
-def job_html(job_number):
+class BisWebUnavailableException(requests.exceptions.RequestException):
+    pass
+
+
+def job_html(job_number, attempt=1):
+    if attempt > 3:
+        print("tried 3 times but could not download job: {}".format(job_number))
+        return None
     try:
         r = requests.get(job_url(job_number), headers=HEADERS)
-        if r.status_code == 200:
+        r.raise_for_status()
+        if UNAVAILABLE in r.text:
+            raise BisWebUnavailableException
+        else:
             print("✓ {}".format(job_number))
             return r.text
-        else:
-            print("request for job: {} failed with status code {}".format(job_number, r.status_code))
-            return None
+    except requests.exceptions.RequestException:
+        print("Request to download job {} failed on attempt {}. Pausing...".format(job_number, attempt))
+        sleep(PAUSE_BEFORE_RETRY)
+        return job_html(job_number, attempt=1)
     except:
+        print("Unexpected error:", sys.exc_info()[0])
         print("failed to download job: {}".format(job_number))
-        return None
+        raise
 
 
 def job_file_path(job_number):
